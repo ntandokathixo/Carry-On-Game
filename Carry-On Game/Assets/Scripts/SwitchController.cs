561817429eaae2fae2f8d4b521617a74de94bb46
Ntandokathixo <ntandodwango@gmail.com>
2026-03-09 02:44:25 +0200

    Sprites Added

--- Carry-On Game/Assets/Scripts/SwitchController.cs
+++ Carry-On Game/Assets/Scripts/SwitchController.cs
@@ -2,14 +2,26 @@ using UnityEngine;
 
 public class SwitchController : MonoBehaviour
 {
-
     public bool goRight = true;
-    
+    public GameObject rightSpriteObject;  // Drag RightSprite here
+    public GameObject leftSpriteObject;    // Drag LeftSprite here
+
+    void Start()
+    {
+        UpdateSprite();
+    }
+
     void OnMouseDown()
     {
         goRight = !goRight;
-        Debug.Log("Switched. Go Right:" + goRight);
+        UpdateSprite();
+        Debug.Log("Switch toggled. Go Right: " + goRight);
     }
 
-    
+    void UpdateSprite()
+    {
+        // Enable the correct sprite, disable the other
+        rightSpriteObject.SetActive(goRight);
+        leftSpriteObject.SetActive(!goRight);
+    }
 }
